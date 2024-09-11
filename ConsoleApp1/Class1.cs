60a019c130e49428c2db7666c7fdd4b666268a7c
hoangphampvh <hoangpham19112002@gmail.com>
2024-09-12 00:29:03 +0700

    k

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
     public class Class1
     {
         public int MyProperty { get; set; }
-        public int My { get; set; }
+        public int Myss { get; set; }
 
     }
 }
