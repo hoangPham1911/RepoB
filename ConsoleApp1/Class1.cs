055eccad137769fa0c4c78bc305cf54b5810a267
hoangphampvh <hoangpham19112002@gmail.com>
2025-03-17 23:48:42 +0700

    point

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     public class Class1
     {
         public int MyProperty { get; set; }
-        public int My123ssss { get; set; }
+        public int My123s { get; set; }
 
     }
 }
